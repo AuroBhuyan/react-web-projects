d85b33cefaacfcbeb2591ef05c83274172dfc37d
AuroBhuyan <aurobhuyan123@gmail.com>
2025-08-19 16:39:14 +0530

    a

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./App.css";
 import RandomColor from './components/RandomColor/RandomColor'
-import Accordian from './components/accordian/Accordian';
+import Accordian from './components/Accordian/Accordian';
 
 const App = () => {
   return (
